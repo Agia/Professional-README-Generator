4ea9ecc29894de562240e1c9e05b8af936275545
Agia <agia@pm.me>
2023-02-16 12:46:06 +0000

    Added questions array content in index.js

--- index.js
+++ index.js
@@ -1,21 +1,80 @@
+// Importing require packages / modules
 const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// array of questions for user
+// Array of questions for user
 const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your repo?'
+    },
 
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Write a short description for your repo:',
+    },
+
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide instructions for installation:',
+        default() {
+            return "No installation required."
+        },
+    },
+
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide usage guidelines:'
+    },
+
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose from the following licenses for your repo:',
+        choices: ['MIT', 'Apache 2.0', 'BSD', 'GPL', 'LGPL'],
+    },
+
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Provide guidelines for contributing to your repo:'
+    },
+
+    {
+        type: 'input',
+        name: 'testing',
+        message: 'Provide instruction for testing:'
+    },
+
+    {
+        type: 'input',
+        name: 'username',
+        message: 'Enter your GitHub username?'
+    },
+
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your contact email address for this repo?'
+    }
 ];
 
-// function to write README file
+// Function to write README file
 function writeToFile(fileName, data) {
 }
 
-// function to initialize program
+// Function to initialize program
 function init() {
+
+
 
 }
 
-// function call to initialize program
+// Function call to initialize program
 init();
